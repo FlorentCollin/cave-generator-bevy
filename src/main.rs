45722aa4f103e591566729bd23d215664017ac78
Florent Collin <florentcollin23@gmail.com>
2022-02-08 21:54:33 +0100

    Drawing a simple square in the center

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,33 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(CaveGeneratorPlugin)
         .run();
+}
+
+struct CaveGeneratorPlugin;
+
+impl Plugin for CaveGeneratorPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_cell)
+            .add_startup_system(setup_camera);
+    }
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn spawn_cell(mut commands: Commands) {
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::WHITE,
+            ..Default::default()
+        },
+        transform: Transform {
+            scale: Vec3::new(40.0, 40.0, 40.0),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
